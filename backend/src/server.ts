98997319a947b0065da0615f82504a568f4c2652
Rahul IB <rahulib7172@gmail.com>
2025-08-05 01:14:31 +0530

    Cors safe

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,13 +10,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URL || process.env.FRONTEND_URL_2 || 'http://localhost:5173',
-//     credentials: true,
-//   })
-// );
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || process.env.FRONTEND_URL_2 || 'http://localhost:5173',
+    credentials: true,
+  })
+);
+// app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
